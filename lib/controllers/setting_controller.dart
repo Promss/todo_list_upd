6dffb321ef6154687215713e826fc03848c959ab
Promss <tima.nurlanbekov@gmail.com>
2022-08-27 19:23:48 +0600

    First Commit

---
+++ lib/controllers/setting_controller.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:todo/utils/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SettingsController extends GetxController {
+  final _storage = GetStorage();
+  final String _nameKey = "name";
+  var username = 'username'.obs;
+
+  static const _uriInsta = 'https://www.instagram.com/espiranto.10';
+  static const _uriGitHub = 'https://github.com/Promss';
+
+  @override
+  void onInit() {
+    super.onInit();
+    readName();
+  }
+
+  void readName() {
+    final nameValue = _storage.read(_nameKey) ?? "username";
+    username.value = nameValue;
+  }
+
+  void saveName(String name) {
+    _storage.write(_nameKey, name);
+    readName();
+  }
+
+  void openSelectedSocialMedia(SocialMedia media) {
+    if (media == SocialMedia.instagram) _openUrl(_uriInsta);
+    if (media == SocialMedia.github) _openUrl(_uriGitHub);
+  }
+
+  void _openUrl(String uri) async {
+    final url = Uri.parse(uri);
+    try {
+      await launchUrl(url);
+    } catch (error) {
+      if (kDebugMode) {
+        print('Could not launch $url');
+      }
+    }
+  }
+}
